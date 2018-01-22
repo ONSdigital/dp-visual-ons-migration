714e18eeac9c5aef3014ac7ad39fc39822d25cb9
Dave Llewellyn <439837@gmail.com>
2018-01-22 09:27:25 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dp-visual-ons-migratior
+
+Script for migration ONS Visual content to ONS website
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ONSdigital/go-ns/log"
+)
+
+func main() {
+	log.Info("dp-visual-migration", nil)
+}
